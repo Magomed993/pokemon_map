d7edf1a025247f9dbfa7e1c3202cd9d10f4818a1
Magomed993 <160238040+Magomed993@users.noreply.github.com>
2024-07-30 00:48:25 +0300

    Added translations of titles

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -74,6 +74,8 @@ def show_pokemon(request, pokemon_id):
     pokemon_image_url = get_image_url(request, requested_pokemon)
     pokemon_data = {
         "title_ru": requested_pokemon.title,
+        "title_en": requested_pokemon.title_en,
+        "title_jp": requested_pokemon.title_jp,
         "img_url": pokemon_image_url,
         "description": requested_pokemon.description
     }
